376961b5e0c77c23eda10c4dd309c9d83dad65e1
Johntae Stroud <johntae.stroud@gmail.com>
2020-04-09 09:28:57 -0400

    refactor

--- script.js
+++ script.js
@@ -25,38 +25,34 @@ function showSuccess(input) {
 }
 
 // check email is valid
-function isValid
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+    //loops thru all inputs, input is the element; it has an id prop
+    inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+        //take in u, e, p, p2, uses the id value
+        showError(input, `${getFieldName(input)} is required`);
+    } else {
+        showSuccess(input);
+    }
+   });
+}
+
+// Get fieldname
+function getFieldName(input) {
+    //capitalize the 1st letter and add the rest of the word
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 //Event listeners - reps an obj that can handle an event dispatched by an EventTarget object
 form.addEventListener('submit', function(e) {
     // prevents the form from submitting
     e.preventDefault();
 
-    if(username.value === '') {
-        // takes in the actual input and the message
-        showError(username, "Username is required");
-    } else {
-        showSuccess(username);
-    }
-
-    if(email.value === '') {
-        // takes in the actual input and the message
-        showError(email, "Email is required");
-    } else {
-        showSuccess(email);
-    }
-
-    if(password.value === '') {
-        // takes in the actual input and the message
-        showError(password, "Password is required");
-    } else {
-        showSuccess(password);
-    }
-
-    if(password2.value === '') {
-        // takes in the actual input and the message
-        showError(password2, "Confirmation password is required");
-    } else {
-        showSuccess(password2);
-    }
+   checkRequired([username, email, password, password2]);
 });
