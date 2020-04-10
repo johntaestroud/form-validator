bedf847f4d36fb20faf3e71ec62b3ae80a486e25
Johntae Stroud <johntae.stroud@gmail.com>
2020-04-10 07:28:06 -0400

    Checking requirements

--- script.js
+++ script.js
@@ -25,24 +25,45 @@ function showSuccess(input) {
 }
 
 // check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
 }
 
 // Check required fields
 function checkRequired(inputArr) {
     //loops thru all inputs, input is the element; it has an id prop
-    inputArr.forEach(function(input) {
-    if(input.value.trim() === '') {
-        //take in u, e, p, p2, uses the id value
-        showError(input, `${getFieldName(input)} is required`);
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            //take in u, e, p, p2, uses the id value
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
     } else {
         showSuccess(input);
     }
-   });
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords do not match");
+    }
+}
 // Get fieldname
 function getFieldName(input) {
     //capitalize the 1st letter and add the rest of the word
@@ -50,9 +71,14 @@ function getFieldName(input) {
 }
 
 //Event listeners - reps an obj that can handle an event dispatched by an EventTarget object
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     // prevents the form from submitting
     e.preventDefault();
 
-   checkRequired([username, email, password, password2]);
+    checkRequired([username, email, password, password2]);
+    //min 3, max 15 for username
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
